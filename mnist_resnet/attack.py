98de499a873ba7f80056e1e01c8b5528a330b3ae
André Vinicius Santos de Lima <81827987+gaspartino@users.noreply.github.com>
2026-06-06 03:43:10 -0300

    Update attack.py

--- mnist_resnet/attack.py
+++ mnist_resnet/attack.py
@@ -380,5 +380,6 @@ for loop_idx in range(args.total_loops):
     for eps in all_eps:
         accuracy_MIM(model, testloader, eps, device, args.normalize)
     
-    for eps in all_eps:
-        accuracy_AutoAttack(model, testloader, num_classes, eps, device, args.normalize)
+    args.ignore_autoattack:
+        for eps in all_eps:
+            accuracy_AutoAttack(model, testloader, num_classes, eps, device, args.normalize)
