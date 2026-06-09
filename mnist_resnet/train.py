cb43cb0d7e5e1c2cc0d6bc3b464fea9767edf607
André Vinicius Santos de Lima <81827987+gaspartino@users.noreply.github.com>
2026-06-09 12:17:52 -0300

    Update train.py

--- mnist_resnet/train.py
+++ mnist_resnet/train.py
@@ -29,7 +29,7 @@ from torchvision.datasets import MNIST, CIFAR10, ImageFolder
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 fc_dim = 64
 args = get_args() 
-seed_torch(random.randint(0, 100))
+#seed_torch(random.randint(0, 100))
 
 device = 'cuda' 
 best_acc = 0 
